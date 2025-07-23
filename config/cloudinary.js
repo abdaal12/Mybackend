beca8580f145f2b09bfb572b4e40beb5c8adfbda
abdaal <aabdaal02@gmail.com>
2025-07-23 19:15:36 +0530

    Initialothe commit

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,10 +1,7 @@
-// config/cloudinary.js
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require("cloudinary").v2;
 cloudinary.config({
-  cloud_name: 'du5q1s3df',
-  api_key: '585357868351496',
-  api_secret: '**********'
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
 module.exports = cloudinary;
-
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -25,7 +25,7 @@ router.get('/:id', getProductById);              // Get product by ID
 
 
 // PROTECTED ROUTES (Require JWT)
-router.post("/", protect, isVendor, upload.single("image"), createProduct);   // Create new product
+router.post("/", upload.single("image"), protect, isVendor,createProduct);   // Create new product
 router.get('/my/products', protect, getMyProducts); // Fetch products created by logged-in user
 router.put('/:id', protect, updateProduct);      // Update product by owner only
 router.delete('/:id', protect, deleteProduct);   // Delete product by owner only
